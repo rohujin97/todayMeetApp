2d644be95389fab8966de217356b5d1019d849d7
rohujin97 <rohujin98@naver.com>
2021-08-20 02:05:17 +0900

    delete local folder and reclone project

--- client/App.js
+++ client/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
 import UserList from './src/component/UserList/UserList'
+import ChatListScreen from './src/component/chat/chatList';
+// import StackUtils from 'stack-utils';
 
-
-export default function App(){
-  return (<UserList/>)
-  // return (<AboutPage/>)
-  // return (<DetailPage/>)
+function App(){
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="ChatList" component={ChatListScreen} options={{ title: '채팅리스트' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
+
+export default App;
--- server/app.js
+++ server/app.js
@@ -1,30 +1,54 @@
 // const createError = require('http-errors');
-const express = require('express')
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
-const moment = require('moment')
-const result = require('./middleware/result')
-const routes = require('./router/common')
-const http = require('http')
-const app = express()
-const server = http.createServer(app)
-const config = require('./config/index')
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const http = require('http');
 const cors = require('cors')
-const CustomError = require('./util/customError')
+const dotenv = require('dotenv')
+const socketio = require('socket.io')
+
+const app = express();
+
+const server = http.createServer(app)
+
+const result = require('./middleware/result');
+const routes = require('./router/common');
+const config = require('./config/index')
+const pool = require('./middleware/pool')
+
 
 if (config.middleware.cors) app.use(cors())
-logger.token('date', () => moment().format("YYYY-MM-DD HH:mm:ss"))
-app.use(logger(config.middleware.morgan))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 //REST API
-app.use('/', routes)
+app.use('/', routes); 
+
+const io = socketio(server)
+// console.log('socket io 요청 받아들일 준비가 됨')
+// require('./middleware/socket')(io)
+// const io = socketio(server, { path: '/io' })
+// io.adapter(redisAdapter({ host: 'localhost', port: 6379 }))
+// require('./middleware/socket')(io)
+
+io.on('connection', function (socket) {
+    console.log('today connected')
+    socket.emit('news', { hello: 'world' })
+
+    socket.on('my other event', function (data) {
+        console.log(data)
+    })
+})
+
+// Load configuration files
+dotenv.config({ path : './env' })
 
 app.use(result[config.middleware.result].notFound) // notFoundError
 app.use(result[config.middleware.result].other) //error handler
+
 server.listen(process.env.PORT || config.port, async () => {
     const startMsg = `${ process.env.PORT || config.port } port is open!!`
     console.info(startMsg)
