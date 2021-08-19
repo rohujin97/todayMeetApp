442fe2e6aa5b9875b673377c031fbb421c60142d
rohujin97 <rohujin98@naver.com>
2021-08-20 02:05:17 +0900

    delete local folder and reclone project git rebase today

---
+++ client/src/component/chat/chatList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const chatList = () => {
+    return (
+        <View>
+            <Text>ChatList Screen</Text>
+        </View>
+    )
+}
+
+export default chatList
