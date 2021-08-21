5fcd1055b9c9a52923fb1c0a89bdf45860304207
rohujin97 <rohujin98@naver.com>
2021-08-21 12:17:14 +0900

    still move page from chatlist to chatroom git rebase3

--- client/src/component/AppTabNavigator/MapTab.js
+++ client/src/component/AppTabNavigator/MapTab.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+<<<<<<< HEAD
 import Loca from '../Loca.js'
+=======
+// import Location from '../Location.js'
+>>>>>>> 7102a15a (still move page from chatlist to chatroom)
 
 export default class MapTab extends Component {
   render() {
     return (
+<<<<<<< HEAD
       <Loca />
+=======
+        <View>
+            <Text>Map</Text>
+        </View>
+    //   <Location />
+>>>>>>> 7102a15a (still move page from chatlist to chatroom)
     );
   }
 }
@@ -16,4 +27,13 @@ const style = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     }
+<<<<<<< HEAD
 });
+=======
+<<<<<<< HEAD
+});
+
+=======
+});
+>>>>>>> 7102a15a (still move page from chatlist to chatroom)
+>>>>>>> 226015e0 (still move page from chatlist to chatroom)
