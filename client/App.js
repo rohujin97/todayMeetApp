3a5d6fc2f2474744e6b4d3106fc6d493dc45f178
alswjd6807 <alswjd6807@naver.com>
2021-08-21 15:46:48 +0900

    conflict 수정

--- client/App.js
+++ client/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { StyleSheet, View, Text, ScrollView, Dimensions } from 'react-native';
 import MainScreen from './src/component/MainScreen';
@@ -5,6 +6,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as Location from 'expo-location';
 import Icon from 'react-native-vector-icons/Ionicons';
+import UserList from './src/component/UserList/UserList'
 
 const deviceWidth = Dimensions.get('window').width;
 const Title = () => {
